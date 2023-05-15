13459380347a0e091356737e6d0e77cc82643833
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-05-15 17:24:00 -0300

    Criando operação de instanciação

--- pedidos-back-end/src/main/java/carreiras/com/github/pedidosbackend/PedidosBackEndApplication.java
+++ pedidos-back-end/src/main/java/carreiras/com/github/pedidosbackend/PedidosBackEndApplication.java
@@ -1,13 +1,34 @@
 package carreiras.com.github.pedidosbackend;
 
+import carreiras.com.github.pedidosbackend.domain.Categoria;
+import carreiras.com.github.pedidosbackend.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class PedidosBackEndApplication {
+public class PedidosBackEndApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PedidosBackEndApplication.class, args);
-	}
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(PedidosBackEndApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(
+                null,
+                "Informática");
+
+        Categoria cat2 = new Categoria(
+                null,
+                "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
