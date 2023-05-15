3d00ea0985d6c6174f8808c727ab0aa5f8d0e15d
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-05-15 15:29:20 -0300

    Primeiro Commit Back End

---
+++ pedidos-back-end/src/main/java/carreiras/com/github/pedidosbackend/PedidosBackEndApplication.java
@@ -0,0 +1,13 @@
+package carreiras.com.github.pedidosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosBackEndApplication.class, args);
+	}
+
+}
